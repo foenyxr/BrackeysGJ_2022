2f108050b36e91c9779dd1df28b8dbf309b04720
napsterfouwaz <fouwaz99@gmail.com>
2022-02-25 23:38:24 +0000

    3D text and Music for scene3

---
+++ Assets/Scripts/Writing.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace First_Main_Scene
+{
+
+    public class Writing : MonoBehaviour
+    {
+
+        public GameObject text;
+        private bool Trigger;
+
+        private void Start()
+
+        {
+            Debug.Log("start");
+            text.SetActive(false);
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (other.gameObject.tag == "Finish")
+            {
+                Debug.Log("enter");
+                text.SetActive(true);
+
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Finish")
+            {
+                Debug.Log("exit");
+                text.SetActive(false);
+
+            }
+        }
+    }
+}
